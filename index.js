0f494501c5ecad22845e85a36da13282722acac3
khaled0915 <khaledsaifullah50956@gmail.com>
2023-10-22 00:52:11 +0600

    hide db user and password using dotenv config

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+require('dotenv').config();
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const cors = require('cors');
 
 const app = express();
@@ -13,6 +18,40 @@ app.use(cors());
 app.use(express.json());
 
 
+// username and pass
+// khaledsaifullah50956
+// gM3Koz0dMH6wQn4s
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4v47x55.mongodb.net/?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
@@ -21,7 +60,7 @@ app.use(express.json());
 
 
 app.get('/' , (req,res)=>{
-    res.send('brand based server is running in the port 5000')
+    res.send('brand based server is  running in the port 5000')
 })
 
 app.listen(port , () =>{
